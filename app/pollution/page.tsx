0b1a00ff72713071bb74bd49c7962157a4da9a41
Yaman Sarabariya <yaman.sarabariya@gmail.com>
2024-05-12 01:07:55 +0545

    Pollution page (#1)
    * fix: add leaflet types
    
    * feat: add pollution page

---
+++ app/pollution/page.tsx
@@ -0,0 +1,8 @@
+import PollutionMainComponent from '@/components/pollution/main'
+import React from 'react'
+
+export default function PollutionPage() {
+  return (
+    <PollutionMainComponent />
+  )
+}
